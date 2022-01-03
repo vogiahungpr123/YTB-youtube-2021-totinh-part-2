fd8ab986136875120c393aa1d452fb3b7cc95d81
vogiahungpr123 <97034482+vogiahungpr123@users.noreply.github.com>
2022-01-03 21:08:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
     introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
-    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    introDesc: `Hưng có điều này muốn hỏi Thư, Thư nhớ phải trả lời thật lòng nha`,
     btnIntro: 'OK',
-    title: 'Anh biết Em thích Anh phải không phải không ? 😙',
-    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
-    btnYes: 'Có, em thật sự thích anh 💖💖💖',
-    btnNo: '💔 Anh mơ à ! 💔 ',
-    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
+    title: 'Thư biết Phát thích Thư phải không phải không ? 😙',
+    desc: 'Thư không trả lời hoặc thoát ra, tức là Thư thích Phát đó nha :> ',
+    btnYes: 'Có, Thư thật sự thích Phát 💖💖💖',
+    btnNo: '💔 Phát mơ à ! 💔 ',
+    question:'Nói cho Phát biết đi vì sao Thư thích Phát 😊😊',
     reasonPlaceholder: 'Lý do',
     btnReply: 'Gởi ! 💌💌',
-    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
+    reply: 'Tại vì Phát đẹp trai đó Ahiiiii 😊😊',
     mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
-    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    messDesc: '💬💬 Nhắn tin cho Phát nha. 💬💬',
     btnAccept: 'Ok lun 💖',
-    messLink: 'https://m.me/PhungQuocDinh'
+    messLink: 'https://www.facebook.com/profile.php?id=100060703703540'
 }
